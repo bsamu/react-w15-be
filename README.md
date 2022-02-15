84a2e0973e1039f9487e542202a80f9ecbe7a347
bsamu <bsamuel0220@gmail.com>
2022-02-15 13:06:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm init -y
+npm i express
+npm i cors
+package.json-ben test helyett devStart: "node index.js" -> így npm run devStart-al futtatja index.js-t, nem kell nézegetni, hogy mit kell futtatni (most node), ha más leklónozza
---
+++ index.js
@@ -0,0 +1,32 @@
+const cors = require('cors')
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 4000
+
+app.use(cors())
+app.use(express.json())
+
+const users = require('./users.json')
+
+app.post('/api/signup', (req, res) => {
+    if (!req.body.name || !req.body.password) {
+        return res.status(400).json('Missing credentials')
+    }
+    const userExists = users.some(user => user.name === req.body.name)
+    if (userExists) {
+        return res.sendStatus(409)
+    }
+    const newUser = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    users.push(newUser)
+
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
