599de3b2cabece51f6194355198937124409239c
bsamu <bsamuel0220@gmail.com>
2022-02-16 11:22:37 +0100

    post, "sign in added"

--- index.js
+++ index.js
@@ -19,12 +19,44 @@ app.post('/api/signup', (req, res) => {
     }
     const newUser = {
         name: req.body.name,
-        password: req.body.password
+        password: req.body.password,
+        todos: []
     }
     users.push(newUser)
 
     fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
     res.sendStatus(200)
+})
+
+app.post('/api/todo', (req, res) => {
+    const autoHead = req.header('authorization')
+    if (!autoHead) return res.sendStatus(401)
+
+    const username = autoHead.split(':::')[0]
+    const password = autoHead.split(':::')[1]
+
+    const user = users.find(user => user.name === username && user.password === password)
+    if (!user) return res.sendStatus(401);
+
+    const todoMess = req.body.todo
+    if (!todoMess) return res.sendStatus(400)
+
+    user.todos.push(todoMess)
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
+    res.sendStatus(200)
+})
+
+app.get('/api/secret', (req, res) => {
+    const autoHead = req.header('authorization')
+    if (!autoHead) return res.sendStatus(401)
+
+    const username = autoHead.split(':::')[0]
+    const password = autoHead.split(':::')[1] // lehetne 2 header, 1 pw, 1 username... ::: azt mi választottuk random, ami valszeg nincs usernameben, pwben, a karakter letiltható
+
+    const user = users.find(user => user.name === username && user.password === password)
+    if (!user) return res.sendStatus(401);
+    res.json({ message: 'valami' })
+    console.log(autoHead)
 })
 
 app.listen(port, () => {
